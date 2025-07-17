2a60bfeba79a15c65433d74008843b56bdcea1fb
yash-vamja <vamjayash532@gmail.com>
2025-07-17 18:04:11 +0530

    my edit

---
+++ minimal/src/components/biro.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function hiro() {
+  return (
+    <div>
+      <p>haygyu tari pela</p>
+      <p>bhagijane</p>
+      <p>hihiiii</p>
+    </div>
+  )
+}
