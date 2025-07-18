39fb90a1e2748df3fc88c1daeb89479b5b9fb734
yash-vamja <vamjayash532@gmail.com>
2025-07-18 16:21:12 +0530

    header file add

---
+++ minimal/src/componest/headerComponet/header.jsx
@@ -0,0 +1,11 @@
+import '../headerComponet/header.css';
+function Header() {
+    return (
+        <>
+            <div className="main-div-header">
+                
+            </div>
+        </>
+    )
+}
+export default Header
