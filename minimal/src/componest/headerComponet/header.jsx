7bfad3a7f4a9e86d5fff4ad48775d31d0c00b69f
yash-vamja <vamjayash532@gmail.com>
2025-07-18 17:48:41 +0530

    header complete

--- minimal/src/componest/headerComponet/header.jsx
+++ minimal/src/componest/headerComponet/header.jsx
@@ -1,9 +1,28 @@
 import '../headerComponet/header.css';
+import logoHeader from '../headerComponet/header-img/logo.png'
 function Header() {
     return (
         <>
             <div className="main-div-header">
-                
+                <div className="inner-div-header">
+                    <div className="inner-logo-div-header">
+                        <img  className='logo-header' src={logoHeader} alt="" />
+                    </div>
+                    <div className="inner-navlist-header">
+                        <ul className='ul-header'>
+                            <li>Home</li>
+                            <li>Service</li>
+                            <li>Feature</li>
+                            <li>Product</li>
+                            <li>testimonial</li>
+                            <li>FAQ</li>
+                        </ul>
+                    </div>
+                    <div className="btn-header">
+                        <button className='login-header-btn'>Login</button>
+                        <button className='signup-header-btn'>Sign up</button>
+                    </div>
+                </div>
             </div>
         </>
     )
