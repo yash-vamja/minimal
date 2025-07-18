333e23bc6765d32dfc6b3f7304a54fc837a39c94
Abhi Vagadiya <your-email@example.com>
2025-07-18 17:44:40 +0530

    Refactor code structure for improved readability and maintainability

--- minimal/src/App.jsx
+++ minimal/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Header from './componest/headerComponet/header'
+import Hirosession from './componest/hirosession/hirosession'
 
 function App() {
   return (
     <>
     <Header />
+    <Hirosession />
     </>
   )
 }
