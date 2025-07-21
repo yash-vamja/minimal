3c42053dfaea8bef41262d2e32422e616704036c
yash-vamja <vamjayash532@gmail.com>
2025-07-21 18:50:34 +0530

    client compo begging

--- minimal/src/App.jsx
+++ minimal/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Header from './componest/headerComponet/header'
 import Hirosession from './componest/hirosession/hirosession'
+import Client from './componest/clientComponet/client'
 
 function App() {
   return (
     <>
     <Header />
     <Hirosession />
+    <Client />
     </>
   )
 }
