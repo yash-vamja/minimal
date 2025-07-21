3e935850395156baa80a8bbfc01ad43a4d8b0ef0
Abhi Vagadiya <your-email@example.com>
2025-07-21 18:55:12 +0530

    Add helping component with stats display and associated styles

--- minimal/src/App.jsx
+++ minimal/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import Header from './componest/headerComponet/header'
+import Helping from './componest/helping/helping'
 import Hirosession from './componest/hirosession/hirosession'
+
 
 function App() {
   return (
     <>
     <Header />
     <Hirosession />
+    <Helping />
     </>
   )
 }
